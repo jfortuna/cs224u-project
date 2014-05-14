9584a1d6a6c26106a06272b419ad896526b8e825
Julie Fortuna <julie.fortuna@gmail.com>
2014-05-14 12:53:34 -0700

    adding scripts, utils

---
+++ scripts/supremecourt.py
@@ -0,0 +1,16 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+
+import readdata
+
+all_utterances = readdata.read_supreme_court()
+
+def bag_of_words():
+    #TODO
+
+def stylistic_features():
+    #TODO
+
+def coordination_features():
+    #TODO
+
---
+++ utils/readdata.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+def read_supreme_court():
+    all_utterances = []
+    with open('../data/supreme_court_dialogs_corpus_v1.01/supreme.conversations.txt', 'r') as f:
+        for line in f:
+            u = {}
+            splits = line.split(' +++$+++ ')
+            u['case_id'] = splits[0]
+            u['utterance_id'] = splits[1]
+            if splits[2] == 'TRUE':
+                u['after_prev'] = True
+            else:
+                u['after_prev'] = False
+            u['speaker'] = splits[3]
+            if splits[4] == 'JUSTICE':
+                u['is_justice'] = True
+            else:
+                u['is_justice'] = False
+            u['justice_vote'] = splits[5]
+            u['presentation_side'] = splits[6]
+            u['utterance'] = splits[7]
+            all_utterances.append(u)
+    print len(all_utterances)
+    return all_utterances
