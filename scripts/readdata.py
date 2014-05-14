9691725409ee3532d34a17f8880c3cd403fcb96b
Alice Su-Chin Yeh <aliceyeh@corn20.stanford.edu>
2014-05-14 15:59:18 -0700

    generated random valued vectors, used sklearn's svm to classify

--- scripts/readdata.py
+++ scripts/readdata.py
@@ -22,5 +22,16 @@ def read_supreme_court():
             u['presentation_side'] = splits[6]
             u['utterance'] = splits[7]
             all_utterances.append(u)
+
+
     print len(all_utterances)
+    
     return all_utterances
+
+
+utterances = read_supreme_court()
+
+# for u in xrange(0, 9):
+#     print utterances[u]
+
+# for speaker in utterances
