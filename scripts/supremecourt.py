c8488fb1702a947c9badc0a65d96b5c6c5228028
Julie Fortuna <julie.fortuna@gmail.com>
2014-05-14 13:35:42 -0700

    python init so importing works hopefully

--- scripts/supremecourt.py
+++ scripts/supremecourt.py
@@ -1,16 +1,19 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
 
-import readdata
-
-all_utterances = readdata.read_supreme_court()
+import sys
+import os
+import utils
 
 def bag_of_words():
-    #TODO
+    all_utterances = readdata.read_supreme_court()
 
 def stylistic_features():
+    pass
     #TODO
 
 def coordination_features():
+    pass
     #TODO
 
+bag_of_words()
