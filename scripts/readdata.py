b3ee7092904593cc1bf0f88577725f71c08bb729
Julie Fortuna <julie.fortuna@gmail.com>
2014-05-14 22:51:31 -0700

    also read in supreme court speaker pairs

--- scripts/readdata.py
+++ scripts/readdata.py
@@ -2,13 +2,15 @@ import sys
 import os
 
 def read_supreme_court():
-    all_utterances = []
+    all_utterances = {}
+    speaker_pairs = {}
     with open('../data/supreme_court_dialogs_corpus_v1.01/supreme.conversations.txt', 'r') as f:
+        prev_utterance_id = 0
         for line in f:
             u = {}
             splits = line.split(' +++$+++ ')
             u['case_id'] = splits[0]
-            u['utterance_id'] = splits[1]
+            utterance_id = int(splits[1])
             if splits[2] == 'TRUE':
                 u['after_prev'] = True
             else:
@@ -20,15 +22,24 @@ def read_supreme_court():
                 u['is_justice'] = False
             u['justice_vote'] = splits[5]
             u['presentation_side'] = splits[6]
-            u['utterance'] = splits[7]
-            all_utterances.append(u)
+            u['utterance'] = splits[7].strip()
+            all_utterances[utterance_id] = u
+
+            if u['after_prev']:
+                prev_speaker = all_utterances[prev_utterance_id]['speaker']
+                speaker_pair = (prev_speaker, u['speaker'])
+                speaker_pair_ids = (prev_utterance_id, utterance_id)
+                if speaker_pair not in speaker_pairs:
+                    speaker_pairs[speaker_pair] = [speaker_pair_ids]
+                else:
+                    new_ids = speaker_pairs[speaker_pair]
+                    new_ids.append(speaker_pair_ids)
+                    speaker_pairs[speaker_pair] = new_ids
+            prev_utterance_id = utterance_id
     print "Supreme Court data read"
-    return all_utterances
+    return (all_utterances, speaker_pairs)
 
 
-#utterances = read_supreme_court()
+all_utterances, speaker_pairs = read_supreme_court()
+print len(speaker_pairs.keys())
 
-# for u in xrange(0, 9):
-#     print utterances[u]
-
-# for speaker in utterances
