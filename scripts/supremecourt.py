f531d86a66bb886ec9eeb33cdaf2f9fec97894c4
Julie Fortuna <julie.fortuna@gmail.com>
2014-05-15 23:21:58 -0700

    helper functions to tokenize an utterance and to get feature counts from the liwc features

--- scripts/supremecourt.py
+++ scripts/supremecourt.py
@@ -5,18 +5,11 @@ from nltk import tokenize
 import sys
 import os
 import readdata
-import liwc
+import utils
 
 def bag_of_words():
-    all_utterances, speaker_pairs = readdata.read_supreme_court()
-    test = all_utterances[2]['utterance']
-    print test
-    sent_tokens = tokenize.sent_tokenize(test)
-    for sent_token in sent_tokens:
-        words = tokenize.word_tokenize(sent_token)
-        print words
-        if liwc.articles & set(words):
-            print "articles"
+    pass
+    #TODO
 
 def stylistic_features():
     pass
@@ -26,4 +19,9 @@ def coordination_features():
     pass
     #TODO
 
-bag_of_words()
+
+#testing get_liwc_counts_from_utterances
+all_utterances, speaker_pairs = readdata.read_supreme_court()
+test = all_utterances[2]['utterance']
+print test
+print utils.get_liwc_counts_from_utterance(test)
---
+++ scripts/utils.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import liwc
+
+from nltk import tokenize
+
+def get_liwc_counts_from_utterance(utterance):
+    tokens = tokenize_utterance(utterance)
+    num_articles, num_aux_verbs, num_conjunctions, num_adverbs, num_ipronouns, num_ppronouns, num_prepositions, num_quantifiers = [0 for x in range(8)]
+    for token in tokens:
+        if token in liwc.articles:
+            num_articles += 1
+        if token in liwc.aux_verbs:
+            num_aux_verbs += 1
+        if token in liwc.conjunctions:
+            num_conjunctions += 1
+        if token in liwc.adverbs:
+            num_adverbs += 1
+        if token in liwc.ipronouns:
+            num_ipronouns += 1
+        if token in liwc.ppronouns:
+            num_pprounouns += 1
+        if token in liwc.prepositions:
+            num_prepositions += 1
+        if token in liwc.quantifiers:
+            num_quantifiers += 1
+    return (num_articles, num_aux_verbs, num_conjunctions, num_adverbs, num_ipronouns, num_ppronouns, num_prepositions, num_quantifiers)
+
+def tokenize_utterance(utterance):
+    token_list = []
+    sent_tokens = tokenize.sent_tokenize(utterance)
+    for sent_token in sent_tokens:
+        words = tokenize.word_tokenize(sent_token)
+        token_list.append(words)
+    return [item for sublist in token_list for item in sublist]
