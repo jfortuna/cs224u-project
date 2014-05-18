b051029b2af11c1291c3b9506a9291cdb444ec65
Alice Yeh <alice.s.yeh@gmail.com>
2014-05-18 12:53:23 -0700

    nv, not using supremecourt_utils.py cuz will have not access to global variables

--- scripts/supremecourt.py
+++ scripts/supremecourt.py
@@ -25,7 +25,7 @@ def stylistic_features(all_speaker_pairs):
 	pass
 #TODO
 
-def coordination_features(all_speaker_pairs):
+def coordination_features(all_speaker_pairs, all_utterances):
 	#TODO Macro-Averaging C(b, A)
 	print "Takes about ~2min to finish.....grab a cup of coffee"
 	allscores = {}
@@ -44,30 +44,102 @@ def coordination_features(all_speaker_pairs):
 		else:
 			label = low
 		svm_vector.append(label)
-		svm_vector.append(speaker_pair_coordination(pair, conversation))
+		svm_vector.append(supremecourt_utils.speaker_pair_coordination(pair, conversation))
 		allscores[pair] = svm_vector
 		# print allscores[pair]
 
 	return allscores
 
+def speaker_pair_coordination(speaker_pair, conversation):
+	# print speaker_pair
+	b_speaker = speaker_pair[1]
+	a_target = speaker_pair[0]
 
-# svm_vector = [0] * 10
-#     	label = error
-#     	if (pair[0].find('JUSTICE') > -1 and pair[1].find('JUSTICE') == -1):
-#     		label = high
-#     	if (pair[0].find('CHIEF') > -1 and pair[1].find('CHIEF') == -1):
-#     		label = high
-#     	else:
-#     		label = low
-#     	svm_vector.append(str(pair))
-#     	svm_vector.append(label)
-#     	print svm_vector
-    	# C_score = tuple(speaker_pair_coordination(pair, conversation))
-    	# svm_vector.append(C_score)
-    	# allscores[pair] = svm_vector
+	b_coord_a_counts = (0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+	b_exhibits_counts = (0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+	a_exhibits_counts = (0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+
+	for exchange in conversation:
+		curr_coord = count_coordination(exchange)		
+		b_coord_a_counts = tuple(numpy.array(b_coord_a_counts) + numpy.array(curr_coord))
+		
+		b_curr_exhibits = count_exhibits_feature(exchange, 1)
+		a_curr_exhibits = count_exhibits_feature(exchange, 0)
+
+		b_exhibits_counts = tuple(numpy.array(b_exhibits_counts) + numpy.array(b_curr_exhibits))
+		a_exhibits_counts = tuple(numpy.array(a_exhibits_counts) + numpy.array(a_curr_exhibits))
+
+	return calc_coordination(len(conversation), b_coord_a_counts, b_exhibits_counts, a_exhibits_counts)
 
 
+def count_coordination(utterance_pair):
+	b_utterance = all_utterances[utterance_pair[1]]['utterance']
+	a_utterance = all_utterances[utterance_pair[0]]['utterance']
 
+	b_utter_vec = utils.get_liwc_counts_from_utterance(b_utterance)
+	a_utter_vec = utils.get_liwc_counts_from_utterance(a_utterance)
+
+	coordination_counts = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+	for marker_id in range(0,8):
+		
+		if (a_utter_vec[marker_id] > 0) and (b_utter_vec[marker_id] > 0):
+			coordination_counts[marker_id] = coordination_counts[marker_id] + 1.0;
+
+	return coordination_counts
+
+def count_exhibits_feature(utterance_pair, speaker):
+	utterance = all_utterances[utterance_pair[speaker]]['utterance']
+	utter_vec = utils.get_liwc_counts_from_utterance(utterance)
+
+	exhibits_feature_counts = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+	for marker_id in range(0,8):
+		
+		if utter_vec[marker_id] > 0:
+			exhibits_feature_counts[marker_id] = exhibits_feature_counts[marker_id] + 1.0;
+	return exhibits_feature_counts
+
+
+def calc_coordination(num_exchange, coordination_counts, b_exhibits_counts, a_exhibits_counts):
+	# print coordination_counts
+	# print b_exhibits_counts
+	# print a_exhibits_counts
+
+	coordination_prob = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+	for i in xrange(0, len(coordination_counts)):
+		if coordination_counts[i] == 0.0:
+			coordination_prob[i] = 0.0
+		else:
+			coordination_prob[i] = coordination_counts[i] / a_exhibits_counts[i];
+
+	# coordination_prob = tuple(numpy.array(coordination_counts) / numpy.array(a_exhibits_counts));
+	b_exhibits_prob = tuple(numpy.array(b_exhibits_counts) / num_exchange);
+
+	coord_prob = tuple(numpy.array(coordination_prob) - numpy.array(b_exhibits_prob))
+	# print coord_prob
+	return coord_prob
+
+
+def speaker_pair_sum_vectors(speaker_pair):
+	b_a_sum= {}
+
+	b_speaker = speaker_pair[1]
+	a_target = speaker_pair[0]
+
+	b_a_sum[b_speaker] = (0,0,0,0,0,0,0,0)
+	b_a_sum[a_target] = (0,0,0,0,0,0,0,0)
+
+	conversation = speaker_pairs[speaker_pair]
+	for exchange in conversation:
+		b_utterance = all_utterances[exchange[1]]['utterance']
+		a_utterance = all_utterances[exchange[0]]['utterance']
+
+		b_utter_vec = utils.get_liwc_counts_from_utterance(b_utterance)
+		a_utter_vec = utils.get_liwc_counts_from_utterance(a_utterance)
+
+		b_a_sum[b_speaker] = tuple(numpy.array(b_a_sum[b_speaker]) + numpy.array(b_utter_vec)) 
+		b_a_sum[a_target] = tuple(numpy.array(b_a_sum[a_target]) + numpy.array(a_utter_vec)) 
+
+	return b_a_sum
 #testing get_liwc_counts_from_utterances
 all_utterances, speaker_pairs = readdata.read_supreme_court()
 # test = all_utterances[2]['utterance']
