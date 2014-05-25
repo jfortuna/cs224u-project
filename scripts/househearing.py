b8388d88ca21d6ecf5f2a737641c31d2824aa408
Alice Yeh <alice.s.yeh@gmail.com>
2014-05-25 13:44:53 -0700

    finished reading csv file

--- scripts/househearing.py
+++ scripts/househearing.py
@@ -9,12 +9,13 @@ def build_vectors():
     for hearing in house_utterances:
         hearing_map = {}
         for speaker, list_of_utterances in hearing.iteritems():
-            all_vectors[][speaker] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            hearing_map[speaker] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
             for utterance in list_of_utterances:
                 # print utterance
                 new_vector = utils.get_liwc_counts_from_utterance(utterance)
                 # print new_vector
-                all_vectors[speaker] = map(add,  all_vectors[speaker], list(new_vector))
+                hearing_map[speaker] = map(add,  hearing_map[speaker], list(new_vector))
+        all_vectors.append(hearing_map)
     return all_vectors
 
 
@@ -22,9 +23,18 @@ def pair_rank(raw_vectors):
     pair_data = []
     pair_target = []
     for hearing in raw_vectors:
-        combos  = combinations(raw_vectors.keys(), 2)
-    for combo in combos:
-        new_instance = raw_vectors[]
+        combos  = combinations(hearing.keys(), 2)
+        for combo in combos:
+            person1 = combo[0]
+            person2 = combo[1]
+            new_instance = hearing[person1] + hearing[person2]
+            pair_data.append(new_instance)
+            rel_rank = rank_lookup(person1, person2, year)
+            if rel_rank!= 0: pair_target.append(rel_rank)
+
+
+def rank_lookup(x,y, year):
+    
 
 
 house_utterances = readdata.read_house_hearing('../../data/small_house/')
