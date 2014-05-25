c25a2b4f20e6d3d4945774f1a0384274ce41d2ee
Julie Fortuna <julie.fortuna@gmail.com>
2014-05-25 14:41:45 -0700

    read in csv as unicode

--- scripts/househearing.py
+++ scripts/househearing.py
@@ -5,6 +5,8 @@ from itertools import combinations
 import csv
 import collections
 import os
+import codecs
+import cStringIO
 
 def build_vectors():
     all_vectors = []
@@ -45,12 +47,40 @@ def read_rank_data(dirname = 'rank/'):
     for filename in os.listdir(base_path):
         year = filename[:4]
         with open(base_path + filename) as csvfile:
-            reader = csv.reader(csvfile, delimiter = ",")
+            reader = UnicodeReader(csvfile, delimiter = ",")
             for row in reader:
                 all_rank[year][row[1]] = row[0]
     return all_rank
 
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
 
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
 
 
 all_rank = read_rank_data()
