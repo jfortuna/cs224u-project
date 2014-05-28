454a26be7c4bed84488ffd1a16a7b4b269005c76
Alice Yeh <alice.s.yeh@gmail.com>
2014-05-28 13:00:06 -0700

    get_avg_length and get_sum_length now return a list rather than a number

--- scripts/househearing.py
+++ scripts/househearing.py
@@ -28,13 +28,13 @@ import sys
 sys.stdout = codecs.getwriter('utf-8')(sys.__stdout__)
 
 def get_avg_utterance_length(num_utterances, combined_utterances):
-    return len(combined_utterances.split())/num_utterances;
+    return [len(combined_utterances.split())/num_utterances]
 
 def get_liwc_features(combined_utterances):
     return list(utils.get_liwc_counts_from_utterance(combined_utterances))
 
 def get_sum_utterance_length(combined_utterances):
-    return len(combined_utterances.split())
+    return [len(combined_utterances.split())]
 
 def build_vectors():
     all_vectors = []
@@ -43,7 +43,9 @@ def build_vectors():
         hearing_map = {}
         for speaker, utterances in hearing.iteritems():
             combined_utterances = ' '.join(utterances)
-            hearing_map[speaker] = get_liwc_features_of_interest(combined_utterances, ['singppronouns', 'pluralppronouns'])
+            hearing_map[speaker] = get_avg_utterance_length(len(utterances), combined_utterances) + \
+                                get_sum_utterance_length(combined_utterances) + \
+                                utils.get_liwc_features_of_interest(combined_utterances, ['singppronouns', 'pluralppronouns'])
             #get_avg_utterance_length + get_liwc_features + get_sum_utterance_length
         all_vectors.append(hearing_map)
     return all_vectors
